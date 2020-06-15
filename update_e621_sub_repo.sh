ac444e9ae842ceeb018ab3289cdc631b31c70451
noirscape <neko@catgirlsin.space>
2020-06-15 13:41:24 +0200

    commit rev-remove

--- update_e621_sub_repo.sh
+++ update_e621_sub_repo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Note: requires git subrepo to be installed.
 rm core/e621_code/REVISION
+git commit core/e621_code/REVISION -m "Delete REVISION file"
 git subrepo pull core/e621_code
 git rev-parse --short HEAD > core/e621_code/REVISION
 git commit core/e621_code/REVISION -m "Add REVISION file"
